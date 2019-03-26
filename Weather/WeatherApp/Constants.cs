da2012f60d2322d651cf7c15011393217be58527
Steve Bilogan <steve.bilogan@gmail.com>
2019-03-25 22:22:28 -0400

    Add Xamarin.Essentials to grab location

--- Weather/WeatherApp/Constants.cs
+++ Weather/WeatherApp/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-        public static string OpenWeatherMapAPIKey = "INSERT_API_KEY_HERE";
+        public static string OpenWeatherMapAPIKey = "42da918334595214eeec39a45b1d423e";
     }
 }
--- Weather/WeatherApp/MainPage.xaml.cs
+++ Weather/WeatherApp/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Essentials;
+using System.Diagnostics;
 
 namespace WeatherApp
 {
@@ -10,23 +13,42 @@ namespace WeatherApp
         public MainPage()
         {
             InitializeComponent();
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
             _restService = new RestService();
         }
 
         async void OnGetWeatherButtonClicked(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            try
             {
-                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+                else
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Default);
+                    location = await Geolocation.GetLocationAsync(request);
+                }
+
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint, location.Latitude, location.Longitude));
                 BindingContext = weatherData;
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Boop beep: {ex}");
+            }
+           
         }
 
-        string GenerateRequestUri(string endpoint)
+        string GenerateRequestUri(string endpoint, double lat, double lon)
         {
+            // All of this hurts my soul
             string requestUri = endpoint;
-            requestUri += $"?q={_cityEntry.Text}";
-            requestUri += "&units=imperial"; // or units=metric
+            requestUri += $"?lat={lat}&lon={lon}";
+            requestUri += "&units=metric"; // or units=metric
             requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
             return requestUri;
         }
